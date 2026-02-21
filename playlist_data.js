3f45d907335712916ecd364131df5bc87028d78d
cha <rose3470@naver.com>
2026-02-21 13:19:32 +0900

    add b

---
+++ playlist_data.js
@@ -0,0 +1,92 @@
+const playlists = {
+    "enka": [
+        {
+            "title": "이성애 - 단장의미아리고개",
+            "url": "music/enka/이성애 - 단장의미아리고개.mp3"
+        },
+        {
+            "title": "이성애 - 돌아와요부산항에",
+            "url": "music/enka/이성애 - 돌아와요부산항에.mp3"
+        },
+        {
+            "title": "이성애 - 물방아도는내력",
+            "url": "music/enka/이성애 - 물방아도는내력.mp3"
+        },
+        {
+            "title": "이성애-물방아도는내력",
+            "url": "music/enka/이성애-물방아도는내력.mp3"
+        },
+        {
+            "title": "주현미 - 스물살때 어느밤",
+            "url": "music/enka/주현미 - 스물살때 어느밤.mp3"
+        },
+        {
+            "title": "주현미 - 정말 좋았네",
+            "url": "music/enka/주현미 - 정말 좋았네.mp3"
+        },
+        {
+            "title": "주현미 - 정주지않으리",
+            "url": "music/enka/주현미 - 정주지않으리.mp3"
+        }
+    ],
+    "jazz": [
+        {
+            "title": "금잔디 - 고장난벽시계",
+            "url": "music/jazz/금잔디 - 고장난벽시계.mp3"
+        },
+        {
+            "title": "금잔디 - 남자라는이유로",
+            "url": "music/jazz/금잔디 - 남자라는이유로.mp3"
+        },
+        {
+            "title": "금잔디 - 네박자",
+            "url": "music/jazz/금잔디 - 네박자.mp3"
+        },
+        {
+            "title": "금잔디 - 안동역에서",
+            "url": "music/jazz/금잔디 - 안동역에서.mp3"
+        },
+        {
+            "title": "금잔디 - 일편단심",
+            "url": "music/jazz/금잔디 - 일편단심.mp3"
+        }
+    ],
+    "old": [
+        {
+            "title": "Agnetha Fältskog - Back On Your Radio",
+            "url": "music/old/Agnetha Fältskog - Back On Your Radio.mp3"
+        },
+        {
+            "title": "Agnetha Fältskog - When You Really Loved Someone",
+            "url": "music/old/Agnetha Fältskog - When You Really Loved Someone.mp3"
+        },
+        {
+            "title": "Beyonce - TEXAS HOLD EM",
+            "url": "music/old/Beyonce - TEXAS HOLD EM.mp3"
+        },
+        {
+            "title": "Daniel - Blackboard of My Heart",
+            "url": "music/old/Daniel - Blackboard of My Heart.mp3"
+        },
+        {
+            "title": "Daniel - Pretty Little Girl From Omagh",
+            "url": "music/old/Daniel - Pretty Little Girl From Omagh.mp3"
+        },
+        {
+            "title": "Isla Grant - Every Moment Of Every Hour",
+            "url": "music/old/Isla Grant - Every Moment Of Every Hour.mp3"
+        },
+        {
+            "title": "Isla Grant - Only Yesterday",
+            "url": "music/old/Isla Grant - Only Yesterday.mp3"
+        },
+        {
+            "title": "Mary Duff - Just Lovin' You",
+            "url": "music/old/Mary Duff - Just Lovin' You.mp3"
+        },
+        {
+            "title": "Mary Duff - Power Of Love",
+            "url": "music/old/Mary Duff - Power Of Love.mp3"
+        }
+    ]
+};
